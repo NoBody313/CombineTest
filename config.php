de7fd26fbe814d2323b44d101132e92ef6b98f91
NoBody313 <muhammadfatihmaulana434@gmail.com>
2023-10-30 13:06:50 +0700

    test

--- config.php
+++ config.php
@@ -11,5 +11,5 @@ $tenant_id = 'd7d09513-a3f5-401c-9685-34ab6c552453';
 $authorization_header_value = 'supersecretauthheader';
 
 // update if you aren't running the app or FusionAuth in the default location 
-$redirect_uri = 'https://79a0-103-233-100-232.ngrok-free.app/oauth-callback.php';
+$redirect_uri = 'https://seashell-app-7txxu.ondigitalocean.app/oauth-callback.php';
 $fa_url = 'http://146.190.192.76:9011';
