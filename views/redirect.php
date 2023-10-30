b4443d6049b8ab1a08e160cffac523f3f5ca773c
NoBody313 <muhammadfatihmaulana434@gmail.com>
2023-10-30 13:54:55 +0700

    update dikit

--- views/redirect.php
+++ views/redirect.php
@@ -1,3 +1,37 @@
+<?php
+require __DIR__ . '/../config.php';
+require __DIR__ . '/../common.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+use League\OAuth2\Client\Provider\GenericProvider;
+
+$provider = new GenericProvider([
+    'clientId'                => $client_id, 
+    'clientSecret'            => $client_secret,
+    'redirectUri'             => $redirect_uri,
+    'urlAuthorize'            => $fa_url . '/oauth2/authorize',
+    'urlAccessToken'          => $fa_url . '/oauth2/token',
+    'urlResourceOwnerDetails' => $fa_url . '/oauth2/userinfo',
+]);
+
+// Inisialisasi sesi jika belum ada
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Simpan state yang dihasilkan untuk sesi.
+if (!isset($_SESSION['oauth2state'])) {
+    $_SESSION['oauth2state'] = $provider->getState();
+}
+
+if (isset($_SESSION['user'])) {
+    // Jika variabel sesi tidak diatur, pengguna belum login
+    // Redirect pengguna ke halaman login
+    header('Location: https://testingsso.my.id');
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
